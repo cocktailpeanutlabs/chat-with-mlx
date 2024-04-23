3a089528434ab927a8ad61cd4b94867202d57aa6
cocktailpeanut <cocktailpeanuts@proton.me>
2024-04-23 02:28:12 -0400

    init

---
+++ install.js
@@ -0,0 +1,38 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/qnguyen3/chat-with-mlx.git app",
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install -e ."
+        ]
+      }
+    },
+    //  Uncomment this step to add automatic venv deduplication (Experimental)
+    //  {
+    //    method: "fs.link",
+    //    params: {
+    //      venv: "env"
+    //    }
+    //  },
+    {
+      method: "notify",
+      params: {
+        html: "Click the 'start' tab to get started!"
+      }
+    }
+  ]
+}
