b7150914b562fb030a7800c4cc784d85f8d74814
cocktailpeanut <cocktailpeanuts@proton.me>
2024-11-20 15:23:16 -0500

    update

--- install.js
+++ install.js
@@ -17,6 +17,16 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
+          "pip install huggingface_hub==0.25.2"
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
           "pip install -e ."
         ]
       }
